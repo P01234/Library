40b3db6978ea13c848d3de1e82f3b7277c7d9242
Pedro Kauã <pk.bleite@gmail.com>
2022-10-17 17:07:16 -0300

    modalStyling

---
+++ library.js
@@ -0,0 +1,6 @@
+//code everyday, my friend. It's your way up.
+//This project is meant to use object oriented programming, althought not entirely
+document.addEventListener("DOMContentLoaded", function(){
+    //store all books in a simple array.
+    //to store it take the input from users and add it to the array. It'll be stored in functions
+})
