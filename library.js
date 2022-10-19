5253746e672d6388cf9828f200e527891b9d3420
Pedro Kauã <pk.bleite@gmail.com>
2022-10-19 19:55:17 -0300

    ModalWorking

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+
 //code everyday, my friend. It's your way up.
 //This project is meant to use object oriented programming, althought not entirely
 document.addEventListener("DOMContentLoaded", function(){
